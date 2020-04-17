86defbdd50d3feaef5a4cde3de7d20177f160d15
cwjki <ninocasteleiro97@gmail.com>
2020-04-17 00:18:59 -0400

    GET requests

--- models/ninja.js
+++ models/ninja.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create ninja Schema & model
+//create geolocation Schema
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere"
+  }
+});
+
+// create ninja Schema
 const NinjaSchema = new Schema({
   name: {
     type: String,
@@ -13,8 +25,8 @@ const NinjaSchema = new Schema({
   available: {
     type: Boolean,
     default: false
-  }
-  // add in geo location
+  },
+  geometry: GeoSchema
 
 });
 
